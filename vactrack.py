b748a093888ec4a42706a896947f685f252a7f77
thetafferboy <thetafferboy@gmail.com>
2021-01-18 14:17:27 +0000

    Will now fetch data on both 1st and 2nd doses and uses single function to write add these to a string to tweet.

--- vactrack.py
+++ vactrack.py
@@ -23,25 +23,40 @@ from datetime import date, timedelta
 date_to_check = (date.today() - timedelta(2)).isoformat()
 
 # GOV UK data source API:
-data_read = pd.read_csv('https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumAdmissions&metric=cumPeopleVaccinatedFirstDoseByPublishDate&format=csv', delimiter=',')
+data_read = pd.read_csv(
+    'https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumAdmissions&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&format=csv', delimiter=',')
 
-total_vacs = data_read.loc[data_read.date == date_to_check, 'cumPeopleVaccinatedFirstDoseByPublishDate'].values[0]
-perc_rounded = round( ((total_vacs / population_of_uk) * 100),2)
 
-solid_bars_to_print = perc_rounded // perc_per_bar
-empty_bars_to_print = bar_total - solid_bars_to_print
+def AddDataToTweet(dataValue, textValue):
+    dataToAdd = ''
+    total_vacs = data_read.loc[data_read.date == date_to_check, dataValue].values[0]
+    perc_rounded = round( ((total_vacs / population_of_uk) * 100),2)
 
-StringToTweet = 'UK population vaccinated against COVID-19:\n\n'
-while solid_bars_to_print > 0:
-   StringToTweet = StringToTweet + '▓'
-   solid_bars_to_print -=1
+    solid_bars_to_print = perc_rounded // perc_per_bar
+    empty_bars_to_print = bar_total - solid_bars_to_print
 
-while empty_bars_to_print > 0:
-   StringToTweet = StringToTweet + '░'
-   empty_bars_to_print -= 1
+    dataToAdd += textValue
+    while solid_bars_to_print > 0:
+        dataToAdd += '▓'
+        solid_bars_to_print -=1
 
-StringToTweet += ' ' + str(perc_rounded) + '%\n\n'
-StringToTweet += 'As of '+str(date_to_check)+'\n'
-StringToTweet += 'Using first jab data from gov.uk API'
+    while empty_bars_to_print > 0:
+        dataToAdd += '░'
+        empty_bars_to_print -= 1
 
-api.update_status(StringToTweet)
+    dataToAdd += ' ' + str(perc_rounded) + '%\n\n'
+    return dataToAdd
+
+
+def SourceAndSendTweet(stringToTweet):
+    stringToTweet += 'As of '+str(date_to_check)+'\n'
+    stringToTweet += 'Using data from UK Gov API\n'
+    stringToTweet += '#CovidVaccine'
+    print(stringToTweet)
+    api.update_status(stringToTweet)
+
+
+stringToTweet = ''
+stringToTweet += AddDataToTweet('cumPeopleVaccinatedFirstDoseByPublishDate','1st dose of vaccine progress: \n\n')
+stringToTweet += AddDataToTweet('cumPeopleVaccinatedSecondDoseByPublishDate','2nd dose of vaccine progress: \n\n')
+SourceAndSendTweet(stringToTweet)
